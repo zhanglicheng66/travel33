2ebb35c54648d27eda2d9479aa83e9abfd5082ab
zlc <405328994@qq.com>
2019-07-19 12:58:57 +0800

    perfomance

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,17 @@
 <template>
     <ul class="list">
-        <li class="item" v-for='(item,key) of cities' :key="key">{{key}}</li>
+        <li 
+        class="item" 
+        v-for='item of letters' 
+        :key="item"
+        :ref='item'
+        @touchstart='handleTouchStart'
+        @touchmove = "handleTouchMove"
+        @touchEnd ="handleTouchEnd"
+        @click='handleLetterClick'
+        >{{item}}
+        
+        </li>
     </ul>
 </template>
 
@@ -9,6 +20,56 @@
         name:'CityAlphabet',
         props:{
             cities:Object
+        },
+        computed:{
+            letters(){
+                const letters = []
+                for (let i in this.cities){
+                    letters.push(i)
+                }
+                return letters
+            }
+        },
+        updated(){
+            this.startY = this.$refs['A'][0].offsetTop
+        },
+        data(){
+            return {
+                touchStatus:false,
+                startY:0,
+                timer:null
+            }
+            
+        },
+        methods:{
+            handleLetterClick(e){
+
+               this.$emit('change',e.target.innerText)
+            },
+            handleTouchStart(){
+                this.touchStatus = true
+            },
+            handleTouchMove(e){
+                if(this.touchStatus){
+                    if(this.timer){
+                        clearTimeout(this.timer)
+                    }
+                    this.timer =setTimeout(()=>{
+                            // const startY = this.$refs['A'][0].offsetTop
+                        const touchY = e.touches[0].clientY - 79
+                        const index = Math.floor((touchY - this.startY)/20)
+                        // console.log(index)
+                        if(index>=0 && index< this.letters.length){
+                            this.$emit('change',this.letters[index])
+                        }
+                    },16)
+                    
+                    
+                }
+            },
+            handleTouchEnd(){
+                this.touchStatus = false
+            }
         }
     }
 </script>
