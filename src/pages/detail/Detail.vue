20e2bb5d07f6f45d5857b3b4dbb52968de99dda8
zlc <405328994@qq.com>
2019-07-20 11:09:07 +0800

    ajax

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-        <detail-banner></detail-banner>
+        <detail-banner 
+        :sightName="sightName"
+        :bannerImg="bannerImg"
+        :bannerImgs="gallaryImgs"
+        ></detail-banner>
         <detail-header></detail-header>
         <div class="content">
             <detail-list :list='list'></detail-list>
@@ -9,6 +13,7 @@
 </template>
 
 <script>
+    import axios from 'axios'
     import DetailBanner from './components/banner'
     import DetailHeader from './components/Header'
     import DetailList from './components/List'
@@ -21,6 +26,9 @@
         },
         data(){
             return {
+                sightName:'',
+                bannerImg:'',
+                gallaryImgs:[],
                 list:[{
                     title:'成人票',
                     children:[{
@@ -38,6 +46,29 @@
                 },{
                     title:'特惠票'
                 }]
+            }
+        },
+        mounted(){
+            this.getDetailInfo()
+        },
+        methods:{
+            getDetailInfo(){
+                // axios.get('/api/detail.json?id=' + this.$route.params.id)
+                axios.get('/api/detail.json',{
+                    params:{
+                        id:this.$route.params.id
+                    }
+                }).then(this.handleGetDataSucc)
+            },
+            handleGetDataSucc(res){
+                res = res.data
+                if(res.ret && res.data){
+                    const data = res.data
+                    this.sightName = data.sightName
+                    this.bannerImg = data.bannerImg
+                    this.gallaryImgs = data.gallaryImgs
+                    this.list=data.categoryList
+                }
             }
         }
     }
